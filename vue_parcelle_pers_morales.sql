e6c1fc4a8fed0c38d438741fc52d48342f53bd29
pasqal <pasq2al@gmail.com>
2021-04-12 21:53:36 +0200

    modif vue parcelles

--- vue_parcelle_pers_morales.sql
+++ vue_parcelle_pers_morales.sql
@@ -1,13 +1,19 @@
-CREATE OR REPLACE VIEW parcelle_personne_morale.vue_parcelles_pm2
-AS SELECT concat(ppm."Departement", ppm."Code_Direction", to_char(ppm."Code_Commune"::double precision, 'FM099'::text), replace(ppm."Prefixe"::text, ' '::text, '0'::text), lpad(ppm."Section"::text, 2, '0'::text), to_char(ppm."Num_plan"::double precision, 'FM0999'::text)) AS idu_pm,
-    btrim(concat(ppm."Num_Voirie", ' ', ppm."Nature_voie", ' ', ppm."Nom_voie")) AS adresse_terrain,
-    ppm."Nom_Commune" AS "Commune",
-    cod.signification AS droit,
-    ppm."Forme_juridique_abregee",
-    ppm."Denomination",
-    grp.signification AS groupe,
-    ppm."Num_SIREN"
-   FROM parcelles_pers_morales ppm,
-    list_code_de_droits cod,
-    list_grp_pers_morale grp
-  WHERE ppm."Code_droit"::text = cod.code::text AND ppm."Groupe_personne" = grp.groupe;
+CREATE OR REPLACE
+VIEW vue_proprio_parcelles AS
+SELECT
+	concat(ppm."Departement", ppm."Code_Direction", to_char(ppm."Code_Commune"::double PRECISION, 'FM099'::TEXT), REPLACE(ppm."Prefixe"::TEXT, ' '::TEXT, '0'::TEXT), lpad(ppm."Section"::TEXT, 2, '0'::TEXT), to_char(ppm."Num_plan"::double PRECISION, 'FM0999'::TEXT)) AS idu_pm,
+	btrim(concat(ppm."Num_Voirie", ' ', ppm."Nature_voie", ' ', ppm."Nom_voie")) AS adresse_terrain,
+	ppm."Nom_Commune" AS commune,
+	cod.signification AS droit,
+	ppm."Forme_juridique_abregee",
+	ppm."Denomination",
+	grp.signification AS groupe,
+	ppm."Num_SIREN"
+FROM
+	parcelles_pers_morales ppm,
+	list_code_de_droits cod,
+	list_grp_pers_morale grp
+WHERE
+	ppm."Nom_Commune"::TEXT = 'LOC-EGUINER'::TEXT
+	AND ppm."Code_droit"::TEXT = cod.code::TEXT
+	AND ppm."Groupe_personne" = grp.groupe;
